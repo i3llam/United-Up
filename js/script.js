502a63730d5bf3561f5bd9b9b73f00bbd774b3ce
Mostafa S. Allam <40570188+i3llam@users.noreply.github.com>
2022-03-21 15:12:20 +0200

    Upload United Up files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// image viewer
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
+});
+// image viewer
+
+// anination
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+})
+wow.init();
+// anination
